fc1e757478b46f9a69a8d37df3147a0382ad8f72
Guido <molido@hotmail.it>
2020-06-17 16:27:14 +0200

    Terzo Commit

--- js/script.js
+++ js/script.js
@@ -5,18 +5,21 @@ var invia = document.getElementById("invia");
 
 invia.addEventListener("click", function () {
 
+  // assegnazione variabili
+
   var nome = document.getElementById("nome").value;
   var km = document.getElementById("km").value;
   var eta = document.getElementById("eta").value;
+
   // calcolo prezzo
 
   var prezzo1 = km * 0.21;
 
   // calcolo sconto
 
-  if (eta = Minorenne){
+  if (eta == "Minorenne"){
     prezzo1 = prezzo1 - ((prezzo1 * 20) / 100);
-  }else if (eta = Over65){
+  }else if (eta == "Over65"){
     prezzo1 = prezzo1 - ((prezzo1 * 40) / 100);
   }
 
@@ -29,7 +32,7 @@ invia.addEventListener("click", function () {
   document.getElementById("nomeBiglietto").innerHTML = nome;
   document.getElementById("kmBiglietto").innerHTML = km;
   document.getElementById("etaBiglietto").innerHTML = eta;
-  document.getElementById("prezzoBiglietto").innerHTML = prezzo;
+  document.getElementById("prezzoBiglietto").innerHTML = prezzo + "€";
   document.getElementById("carrozza").innerHTML = carrozza;
 
 })
