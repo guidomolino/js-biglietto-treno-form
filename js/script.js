76e90315796ba783daad0439a5c2fc4035e209d8
Guido <molido@hotmail.it>
2020-06-17 16:23:30 +0200

    Secondo Commit

--- js/script.js
+++ js/script.js
@@ -1,28 +1,35 @@
 
-var nome = document.getElementById("nome").value;
-var km = document.getElementById("km").value;
-var eta = document.getElementById("eta").value;
 
-// calcolo prezzo
 
-var prezzo1 = km * 0.21;
+var invia = document.getElementById("invia");
 
-// calcolo sconto
+invia.addEventListener("click", function () {
 
-if (eta = Minorenne){
-  prezzo1 = prezzo1 - ((prezzo1 * 20) / 100);
-}else if (eta = Over65){
-  prezzo1 = prezzo1 - ((prezzo1 * 40) / 100);
-}
+  var nome = document.getElementById("nome").value;
+  var km = document.getElementById("km").value;
+  var eta = document.getElementById("eta").value;
+  // calcolo prezzo
 
-var prezzo = prezzo1.toFixed(2);
+  var prezzo1 = km * 0.21;
 
-// gen carrozza
+  // calcolo sconto
 
-var carrozza = Math.floor (Math.random() * (100000 - 90000) + 90000);
+  if (eta = Minorenne){
+    prezzo1 = prezzo1 - ((prezzo1 * 20) / 100);
+  }else if (eta = Over65){
+    prezzo1 = prezzo1 - ((prezzo1 * 40) / 100);
+  }
 
-document.getElementById("nomeBiglietto").innerHTML = var nome;
-document.getElementById("kmBiglietto").innerHTML = var km;
-document.getElementById("etaBiglietto").innerHTML = var eta;
-document.getElementById("prezzoBiglietto").innerHTML = var prezzo;
-document.getElementById("carrozza").innerHTML = var carrozza;
+  var prezzo = prezzo1.toFixed(2);
+
+  // gen carrozza
+
+  var carrozza = Math.floor (Math.random() * (100000 - 90000) + 90000);
+
+  document.getElementById("nomeBiglietto").innerHTML = nome;
+  document.getElementById("kmBiglietto").innerHTML = km;
+  document.getElementById("etaBiglietto").innerHTML = eta;
+  document.getElementById("prezzoBiglietto").innerHTML = prezzo;
+  document.getElementById("carrozza").innerHTML = carrozza;
+
+})
